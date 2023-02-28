afba4574ab94cbd7722c3b707652fe091a07b6f9
Hyungmo Gu <guhyungm7@gmail.com>
2023-02-27 18:42:05 -0700

    feat: Add notes 'Static methods and properties' #2

--- 1_8_static_methods_and_properties/code/index.php
+++ 1_8_static_methods_and_properties/code/index.php
@@ -1,50 +1,6 @@
 <?php
-    class Animal {
-        public $weight;
-        
-        public function eat() 
-        {
-            var_dump("All animals can eat");
-        }
-
-        public function reproduce()
-        {
-            var_dump("All animals can reproduce");
-        }
-    }
-
-    class Mammal extends Animal {
-        public $heartRate;
-
-        public function breathe()
-        {
-            var_dump("I can breathe because I am a mammal");
-        }
-    }
-
-    class Dog extends Mammal {
-        public $color;
-        
-        public function bark() 
-        {
-            var_dump("I can bark because I am a " . strtolower(get_class()));
-        }
-
-        public function chase()
-        {
-            var_dump("I am chasing a rabbit");
-        }
-    }
+   
 
     header("Content-Type:text/plain", true);
-    $brown = new Dog();
-    $brown->weight = 20;
-    $brown->heartRate = "okay";
-    $brown->color = "brown";
-    var_dump($brown);
-    $brown->bark();
-    $brown->eat();
-    $brown->reproduce();
-    $brown->breathe();
-    $brown->chase();
+
 ?>
